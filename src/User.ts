90b1b269ec6039937b7f548846370a4a5520aa01
radwan <radwan.mohamed@grandmstg.com>
2022-05-30 08:27:28 -0400

    google maps

---
+++ src/User.ts
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker';
+import { Mappable } from './CustomMap';
+export class User implements Mappable {
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    };
+    constructor() {
+        this.name = faker.name.findName();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.latitude())
+        }
+    }
+    markerContent(): string {
+        return `user Name : ${this.name}`;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+import { User } from "./User";
+const user = new User();
+const company = new Company();
+console.log(company);
+console.log(user);
+const customMap = new CustomMap('map');
+customMap.addMarker(user);
+customMap.addMarker(company);
